6306607131c0a60c778134bafcfa3140ab7eadac
Angeli Mittal <angeli.mittal@gmail.com>
2021-04-17 15:52:38 -0500

    reorganize directory

---
+++ data lab/decl.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+#define TMin INT_MIN
+#define TMax INT_MAX
+
+#include "btest.h"
+#include "bits.h"
+
+test_rec test_set[] = {
+/* Copyright (C) 1991-2018 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>.  */
+/* This header is separate from features.h so that the compiler can
+   include it implicitly at the start of every compilation.  It must
+   not itself include <features.h> or any other header that includes
+   <features.h> because the implicit include comes before any feature
+   test macros that may be defined in a source file before it first
+   explicitly includes a system header.  GCC knows the name of this
+   header in order to preinclude it.  */
+/* glibc's intent is to support the IEC 559 math functionality, real
+   and complex.  If the GCC (4.9 and later) predefined macros
+   specifying compiler intent are available, use them to determine
+   whether the overall intent is to support these features; otherwise,
+   presume an older compiler has intent to support these features and
+   define these macros by default.  */
+/* wchar_t uses Unicode 10.0.0.  Version 10.0 of the Unicode Standard is
+   synchronized with ISO/IEC 10646:2017, fifth edition, plus
+   the following additions from Amendment 1 to the fifth edition:
+   - 56 emoji characters
+   - 285 hentaigana
+   - 3 additional Zanabazar Square characters */
+/* Bit manipulations */
+ {"bitAnd", (funct_t) bitAnd, (funct_t) test_bitAnd, 2, "| ~", 8, 1,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"thirdBits", (funct_t) thirdBits, (funct_t) test_thirdBits, 0,
+    "! ~ & ^ | + << >>", 8, 1,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"allEvenBits", (funct_t) allEvenBits, (funct_t) test_allEvenBits, 1,
+    "! ~ & ^ | + << >>", 12, 2,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+{"byteSwap", (funct_t) byteSwap, (funct_t) test_byteSwap, 3,
+     "! ~ & ^ | + << >>", 25, 2,
+    {{TMin, TMax},{0,3},{0,3}}},
+ {"conditional", (funct_t) conditional, (funct_t) test_conditional, 3, "! ~ & ^ | << >>", 16, 3,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"bitParity", (funct_t) bitParity, (funct_t) test_bitParity, 1, "! ~ & ^ | + << >>", 20, 4,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+/* Two's complement arithmetic */
+ {"tmax", (funct_t) tmax, (funct_t) test_tmax, 0, "! ~ & ^ | + << >>", 4, 1,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"sign", (funct_t) sign, (funct_t) test_sign, 1, "! ~ & ^ | + << >>", 10, 2,
+     {{-TMax, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"isLessOrEqual", (funct_t) isLessOrEqual, (funct_t) test_isLessOrEqual, 2,
+    "! ~ & ^ | + << >>", 24, 3,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"isPositive", (funct_t) isPositive, (funct_t) test_isPositive, 1,
+    "! ~ & ^ | + << >>", 8, 3,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"absVal", (funct_t) absVal, (funct_t) test_absVal, 1, "! ~ & ^ | + << >>", 10, 4,
+  {{-TMax, TMax},{TMin,TMax},{TMin,TMax}}},
+/* FP operations */
+ {"float_neg", (funct_t) float_neg, (funct_t) test_float_neg, 1,
+    "$", 10, 2,
+     {{1, 1},{1,1},{1,1}}},
+ {"float_abs", (funct_t) float_abs, (funct_t) test_float_abs, 1,
+    "$", 10, 2,
+     {{1, 1},{1,1},{1,1}}},
+ {"float_f2i", (funct_t) float_f2i, (funct_t) test_float_f2i, 1,
+    "$", 30, 4,
+     {{1, 1},{1,1},{1,1}}},
+  {"", NULL, NULL, 0, "", 0, 0,
+   {{0, 0},{0,0},{0,0}}}
+};
