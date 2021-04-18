905eda9d55ab610b5e77792a45c67375727712ef
Felix Haba Redondo <fhr2592@moore.wot.eecs.northwestern.edu>
2021-04-18 16:56:10 -0500

    netids

--- data lab/bits.c
+++ data lab/bits.c
@@ -1,7 +1,7 @@
 /* 
  * CS:APP Data Lab 
  * 
- * <Please put your name and userid here>
+Names: Angeli Mittal, Felix Haba NETIDs: ..., fhr2592
  * 
  * bits.c - Source file with your solutions to the Lab.
  *          This is the file you will hand in to your instructor.
